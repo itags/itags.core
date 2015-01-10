010f452c8b0444ca29ccc0403c6e8f3e71f71877
Marco Asbreuk <marco@itsasbreuk.nl>
2015-01-10 01:28:34 +0100

    intermediate saving

--- itags.core.js
+++ itags.core.js
@@ -1,13 +1,13 @@
 
 "use strict";
 
-require('js-ext/lib/object.js');
-require('js-ext/lib/string.js');
+require('js-ext');
 require('polyfill/polyfill-base.js');
+require('./css/itags-core.css');
 
 var asyncSilent = require('utils').asyncSilent,
     laterSilent = require('utils').laterSilent,
-    CLASS_CE_HIDDEN_BEFORE_RENDER = 'itag-unrendered',
+    CLASS_ITAG_RENDERED = 'itag-rendered',
     NODE = 'node',
     REMOVE = 'remove',
     INSERT = 'insert',
@@ -41,32 +41,30 @@ var asyncSilent = require('utils').asyncSilent,
     merge = function (sourceObj, targetObj) {
         var name;
         for (name in sourceObj) {
-            if (!(name in targetObj)) {
-                if (name==='init') {
+            if (name==='init') {
 console.info('store init function');
 /*jshint -W083 */
-                    targetObj._init = function() {
+                targetObj._init = function() {
 /*jshint +W083 */
-                        var vnode = targetObj.vnode;
-                        if (!vnode.ce_initialized && !vnode.removedFromDOM) {
-                            sourceObj.init.call(targetObj);
-                            Object.protectedProp(vnode, 'ce_initialized', true);
-                        }
-                    };
-                }
-                else if (name==='destroy') {
+                    var vnode = targetObj.vnode;
+                    if (!vnode.ce_initialized && !vnode.removedFromDOM) {
+                        sourceObj.init.call(targetObj);
+                        Object.protectedProp(vnode, 'ce_initialized', true);
+                    }
+                };
+            }
+            else if (name==='destroy') {
 /*jshint -W083 */
-                    targetObj._destroy = function() {
+                targetObj._destroy = function() {
 /*jshint +W083 */
-                        var vnode = targetObj.vnode;
-                        if (!vnode.removedFromDOM && vnode.ce_initialized) {
-                            sourceObj.destroy.call(targetObj);
-                        }
-                    };
-                }
-                else {
-                    targetObj[name] = sourceObj[name];
-                }
+                    var vnode = targetObj.vnode;
+                    if (!vnode.removedFromDOM && vnode.ce_initialized) {
+                        sourceObj.destroy.call(targetObj);
+                    }
+                };
+            }
+            else {
+                targetObj[name] = sourceObj[name];
             }
         }
     },
@@ -79,6 +77,7 @@ DELAYED_FINALIZE_EVENTS.keys().forEach(function(key) {
 module.exports = function (window) {
 
     var DOCUMENT = window.document,
+        RUNNING_ON_NODE = (typeof global !== 'undefined') && (global.window!==window),
         itagCore, MUTATION_EVENTS, Event, registerDelay, focusManager;
 
     require('vdom')(window);
@@ -102,37 +101,47 @@ module.exports = function (window) {
     itagCore = {
 
         itagFilter: function(e) {
-            return !!e.target.renderCE;
+            return !!e.target._updateUI;
         },
 
-        renderDomElements: function(tagName, renderFn, properties, isParcel) {
+        renderDomElements: function(tagName, updateFn, properties, isParcel) {
             var itagElements = DOCUMENT.getAll(tagName),
                 len = itagElements.length,
                 i, itagElement;
             for (i=0; i<len; i++) {
                 itagElement = itagElements[i];
-                this._upgradeElement(itagElement, renderFn, properties, isParcel);
+                this._upgradeElement(itagElement, updateFn, properties, isParcel);
             }
         },
 
-        defineParcel: function(parcelName, renderFn, properties) {
+        defineParcel: function(parcelName, updateFn, properties) {
             if (parcelName.contains('-')) {
                 console.warn(parcelName+' should not consist of a minus token');
                 return;
             }
-            this._defineElement('i-parcel-'+parcelName, renderFn, properties, true);
+            this._defineElement('i-parcel-'+parcelName, updateFn, properties, true);
         },
 
 
-        defineElement: function(itagName, renderFn, properties) {
+        defineElement: function(itagName) {
             if (!itagName.contains('-')) {
                 console.warn('defineElement: '+itagName+' should consist of a minus token');
                 return;
             }
-            this._defineElement(itagName, renderFn, properties);
+            window.ITAGS[itagName] = function() {
+                return DOCUMENT._createElement(itagName);
+            };
         },
 
-        _defineElement: function(itagName, renderFn, properties, isParcel) {
+        defineItag: function(itagName, updateFn, properties) {
+            if (!itagName.contains('-')) {
+                console.warn('defineItag: '+itagName+' should consist of a minus token');
+                return;
+            }
+            this._defineElement(itagName, updateFn, properties);
+        },
+
+        _defineElement: function(itagName, updateFn, properties, isParcel) {
             itagName = itagName.toLowerCase();
 /*jshint boss:true */
             if (window.ITAGS[itagName]) {
@@ -140,56 +149,108 @@ module.exports = function (window) {
                 console.warn(itagName+' already exists and cannot be redefined');
                 return;
             }
-            (typeof renderFn === 'function') || (renderFn=NOOP);
-            this.renderDomElements(itagName, renderFn, properties, isParcel);
-            window.ITAGS[itagName] = this._createElement(itagName, renderFn, properties, isParcel);
+            (typeof updateFn === 'function') || (updateFn=NOOP);
+            this.renderDomElements(itagName, updateFn, properties, isParcel);
+            window.ITAGS[itagName] = this._createElement(itagName, updateFn, properties, isParcel);
         },
 
-        _createElement: function(itagName, renderFn, properties, isParcel) {
+        _createElement: function(itagName, updateFn, properties, isParcel) {
             var instance = this;
             return function() {
                 var element = DOCUMENT._createElement(itagName);
-                instance._upgradeElement(element, renderFn, properties, isParcel);
+                instance._upgradeElement(element, updateFn, properties, isParcel);
                 return element;
             };
         },
 
-        _upgradeElement: function(element, renderFn, properties, isParcel) {
+        _upgradeElement: function(element, updateFn, properties, isParcel) {
             merge(properties, element);
-            if (isParcel) {
-                merge({renderCE: function() {
+            merge({
+                _updateUI: isParcel ? function() {
 console.info('look if parcel can be rendered');
-                    var vnode = element.vnode;
-                    if (vnode._data) {
-                        if (!vnode.ce_initialized) {
-                            if (typeof element._init==='function') {
-                                element._init();
+                        var vnode = element.vnode;
+                        if (vnode._data) {
+                            if (!vnode.ce_initialized) {
+                                if (typeof element._init==='function') {
+                                    element._init();
+                                }
+                                else {
+                                    Object.protectedProp(vnode, 'ce_initialized', true);
+                                }
+                                element._setRendered();
                             }
-                            else {
-                                Object.protectedProp(vnode, 'ce_initialized', true);
-                            }
-                            element.removeClass(CLASS_CE_HIDDEN_BEFORE_RENDER);
+    console.info('going to render parcel');
+                            updateFn.call(element);
                         }
-console.info('going to render parcel');
-                        renderFn.call(element);
+                    } : updateFn,
+                _injectModel: function(model) {
+                    var instance = this,
+                        stringifiedData;
+                    instance.model = model;
+                    instance._updateUI();
+                    if (RUNNING_ON_NODE) {
+                        // store the modeldata inside an inner div-node
+                        try {
+                            stringifiedData = JSON.stringify(model);
+                            instance.prepend('<span class="itag-data">'+stringifiedData+'</span>');
+                        }
+                        catch(e) {
+                            console.warn(e);
+                        }
                     }
-                }}, element);
-            }
-            else {
-                merge({renderCE: renderFn}, element);
-            }
+                },
+                _retrieveModel: function() {
+                    // try to load the model from a stored inner div-node
+                    var instance = this,
+                        dataNode = instance.getElement('span.itag-data'),
+                        stringifiedData;
+                    if (dataNode) {
+                        try {
+                            stringifiedData = dataNode.getHTML();
+                            instance.model = JSON.parseWithDate(stringifiedData);
+                            dataNode.remove(true);
+                        }
+                        catch(e) {
+                            console.warn(e);
+                        }
+                    }
+                    return instance.model;
+                },
+                _setRendered: function() {
+                    var instance = this;
+                    if (instance.hasClass(CLASS_ITAG_RENDERED)) {
+                        // already rendered on the server:
+                        // bin the sored json-data on the property `model`:
+                        instance.retrieveModel();
+                    }
+                    else {
+                        instance.setClass(CLASS_ITAG_RENDERED, null, null, true);
+                    }
+                    instance._itagReady || (instance._itagReady=window.Promise.manage());
+                    instance._itagReady.fulfill();
+                },
+                model: {}
+            }, element);
             merge(Event.Listener, element);
             // render, but do this after the element is created:
             // in the next eventcycle:
             asyncSilent(function() {
                 (typeof element._init==='function') && element._init();
-                element.renderCE();
-                isParcel || element.removeClass(CLASS_CE_HIDDEN_BEFORE_RENDER);
+                element._updateUI();
+                isParcel || element._setRendered();
                 element.hasClass('focussed') && focusManager(element);
             });
         }
 
     };
+
+    (function(HTMLElementPrototype) {
+        HTMLElementPrototype.itagReady = function() {
+            var instance = this;
+            instance._itagReady || (instance._itagReady=window.Promise.manage());
+            return instance._itagReady;
+        };
+    }(window.HTMLElement.prototype));
 
     DOCUMENT._createElement = DOCUMENT.createElement;
     DOCUMENT.createElement = function(tag) {
@@ -207,7 +268,7 @@ console.info('refreshParcels');
             i, parcel;
         for (i=0; i<len; i++) {
             parcel = list[i];
-            parcel.renderCE();
+            parcel._updateUI();
             parcel.hasClass('focussed') && focusManager(parcel);
         }
     };
@@ -218,7 +279,7 @@ console.info('refreshParcels');
 console.info('itag changed attributes '+e.type);
 console.info(e.changed);
             var element = e.target;
-            element.renderCE();
+            element._updateUI();
             element.hasClass('focussed') && focusManager(element);
         },
         itagCore.itagFilter
