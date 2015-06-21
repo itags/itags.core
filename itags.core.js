d45a892c96060b6fe2d1451652984b726bb72feb
Marco Asbreuk <marco@itsasbreuk.nl>
2015-06-22 00:12:32 +0200

    changed lazybinding

--- itags.core.js
+++ itags.core.js
@@ -956,7 +956,7 @@ module.exports = function (window) {
             // sync, but do this after the element is created:
             // in the next eventcycle:
             async(function(){
-                var needsToBind = domElement.lazyBind || (domElement.getAttr('lazybind')==='true');
+                var needsToBind = (domElement.lazyBind  && (domElement.getAttr('lazybind')!=='false')) || (domElement.getAttr('lazybind')==='true');
                 // only if no modelbinding is needed, we can directly init, sync and make ready,
                 // otherwise we need to make this done by  `bindModel`
                 BINDING_LIST.some(function(value, selector) {
